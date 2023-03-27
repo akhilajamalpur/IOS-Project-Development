dfea2df61c40df55e00f92ee7285969a300cad4a
akhilajamalpur <akhilajamalpur15@gmail.com>
2023-03-27 13:56:50 -0400

    edied

--- Tableview Demo/Tableview Demo/ViewController.swift
+++ Tableview Demo/Tableview Demo/ViewController.swift
@@ -22,7 +22,9 @@ class ViewController: UIViewController {
 }
 extension ViewController: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableItems.count
+        
+            return tableItems.count
+        
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=myTable.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
